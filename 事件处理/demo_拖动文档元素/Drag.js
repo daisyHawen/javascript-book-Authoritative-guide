8c7085c759137c0e18e861fce2245a1735db1fed
daisyHawen <512309453@qq.com>
2016-11-14 10:21:34 +0800

    init

---
+++ 事件处理/demo_拖动文档元素/Drag.js
@@ -0,0 +1,10 @@
+/*
+ *这个模块定义一个drag()函数，它用于mousedown事件处理程序的调用
+ *随后的mousedown事件将移动指定元素，mouseup事件将终止拖动
+ *这些实现能同标准和IE两种事件一起工作
+ *它需要用到本书其他地方介绍的getScrollOffsets
+ *参数：
+ *elementToDrag:接收mouseDown事件的元素或某些包含元素
+ *它必须是绝对定位
+ *它的style.left和style.top值将随着用户的拖动而改变
+ */
